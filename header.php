139e7358f2adfc34b26f9589c524dc6fa4d123b1
Onur Celik <onur.celik@logsign.net>
2012-12-21 14:02:08 +0200

    some fixes

--- header.php
+++ header.php
@@ -1,21 +1,24 @@
- <!doctype html>
+<!doctype html>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta http-equiv=”Content-Type” content=”text/html; charset=utf-8“>
         <title>ready2go title</title>
+
         <!-- css files -->
-        <link rel="stylesheet" href="CSS/reset.css" />
-        <link rel="stylesheet" href="CSS/bootstrap.min.css" />
-        <link rel="stylesheet" href="CSS/style.css" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet/less" type="text/css" href="css/style.less" />
         
         <!-- javaScript files -->
-        <script type="text/javascript" src="JS/jquery.min.js"></script>
-        <script type="text/javascript" src="JS/jquery.easing.js"></script>
-        <script type="text/javascript" src="JS/jquery.color.js"></script>
-        <script type="text/javascript" src="JS/jquery.typewriter.js"></script>
-        <script type="text/javascript" src="JS/jquery.transit.min.js"></script>
-        <script type="text/javascript" src="JS/bootstrap.min.js"></script>
-        <script type="text/javascript" src="JS/scripts.js"></script>
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/less.js"></script>
+        <script type="text/javascript" src="js/jquery.ui.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.js"></script>
+        <script type="text/javascript" src="js/jquery.color.js"></script>
+        <script type="text/javascript" src="js/jquery.typewriter.js"></script>
+        <script type="text/javascript" src="js/jquery.transit.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/scripts.js"></script>
+        
     </head>
 
     <body> <!-- ends in footer.php -->
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php include "header.php";  ?>
-        <p id="typist"></p>
-        <a href="README.md">See Readme File</a>
-        <div id="box"> <!-- this is gonna rotate 30degs on hover -->
-            <h1 id="heading">ready2Go</h1>
-        </div>
+
+	<h1>ready2Go</h1>
+
+
         
 <?php include "footer.php";  ?>
